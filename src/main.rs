374e85ba363a4232c6e35f23ff142c7081238246
Anthony Kalaitzis <anthony.kalaitzis@gmail.com>
2023-09-15 09:35:22 +0930

    Change integrator to use nalgebra structures

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use eos::eos::EquationOfState;
-use ndarray::Array;
 use plotly::{
     common::Title,
     {Plot, Scatter},
@@ -13,7 +12,7 @@ const H_STEP: f64 = 0.001;
 const SOLAR_MASS: f64 = 1.4766;
 
 fn main() {
-    let eos_path = std::path::Path::new("config/processed/qmc700_with_crust.csv");
+    let eos_path = std::path::Path::new("config/qmc700_with_crust.csv");
     let mut eos = EquationOfState::new(eos_path).expect("Failed to open Equation of state file");
 
     let indices = 0..(eos.eos.len() - 1);
@@ -22,35 +21,35 @@ fn main() {
         eos.eos[idx].pressure *= INV_KM_2;
     }
 
-    let ns_initial_pressure_guesses = Array::logspace(40.0, -1.1, 1.5, NUM_NS);
-    let mut star_masses: Vec<f64> = Vec::with_capacity(NUM_NS);
-    let mut star_radii: Vec<f64> = Vec::with_capacity(NUM_NS);
-    let mut t = H_STEP;
-    let f = |r: &_, y: &_| eos.dmdr(r, y);
+    // let ns_initial_pressure_guesses = Array::logspace(40.0, 0.1, 1.5, NUM_NS);
+    // let mut star_masses: Vec<f64> = Vec::with_capacity(NUM_NS);
+    // let mut star_radii: Vec<f64> = Vec::with_capacity(NUM_NS);
+    // let mut t = H_STEP;
+    // let f = |r: &_, y: &_| eos.dmdr(r, y);
 
-    for ns_p_guess in ns_initial_pressure_guesses {
-        let m0 = 1.0e-10;
-        let mut y0 = vec![ns_p_guess * INV_KM_2, m0];
+    // for ns_p_guess in ns_initial_pressure_guesses {
+    //     let m0 = 1.0e-10;
+    //     let mut y0 = vec![ns_p_guess * INV_KM_2, m0];
 
-        while y0[0] >= 0.0 {
-            y0 = numerical_integration::euler(&f, &H_STEP, &t, &y0);
-            t += H_STEP;
-        }
+    //     while y0[0] >= 0.0 {
+    //         y0 = numerical_integration::euler(&f, &H_STEP, &t, &y0);
+    //         t += H_STEP;
+    //     }
 
-        star_masses.push(y0[1] / SOLAR_MASS);
-        star_radii.push(t);
-    }
+    //     star_masses.push(y0[1] / SOLAR_MASS);
+    //     star_radii.push(t);
+    // }
 
-    // Create a scatter plot with the data
-    let scatter = Scatter::new(star_radii, star_masses).name("Mass Radius Relation");
+    // // Create a scatter plot with the data
+    // let scatter = Scatter::new(star_radii, star_masses).name("Mass Radius Relation");
 
-    // Create a plot and add the scatter plot
-    let mut plot = Plot::new();
-    plot.add_trace(scatter);
+    // // Create a plot and add the scatter plot
+    // let mut plot = Plot::new();
+    // plot.add_trace(scatter);
 
-    // Customize the plot layout
-    plot.set_layout(plotly::Layout::new().title(Title::from("Mass Radius Relation")));
+    // // Customize the plot layout
+    // plot.set_layout(plotly::Layout::new().title(Title::from("Mass Radius Relation")));
 
-    // Show the plot in a browser window
-    plot.show();
+    // // Show the plot in a browser window
+    // plot.show();
 }
--- src/numerical_integration.rs
+++ src/numerical_integration.rs
@@ -1,12 +1,62 @@
-pub fn euler(
-    f: &dyn Fn(&f64, &Vec<f64>) -> Vec<f64>,
-    h: &f64,
-    t0: &f64,
-    y0: &Vec<f64>,
-) -> Vec<f64> {
-    let mut out = Vec::with_capacity(y0.len());
-    for (f_val, y) in std::iter::zip(f(t0, y0), y0) {
-        out.push(h * f_val + y);
+use std::usize;
+
+use nalgebra::SVector;
+
+pub fn euler<const D: usize>(
+    f: &dyn Fn(f64, SVector<f64, D>) -> SVector<f64, D>,
+    h: f64,
+    t0: f64,
+    y0: SVector<f64, D>,
+) -> SVector<f64, D> {
+    y0 + f(t0, y0) * h
+}
+
+pub fn rk4<const D: usize>(
+    f: &dyn Fn(f64, SVector<f64, D>) -> SVector<f64, D>,
+    h: f64,
+    t0: f64,
+    y0: SVector<f64, D>,
+) -> SVector<f64, D> {
+    let k1 = f(t0, y0);
+    let k2 = f(t0 + (h / 2.0), y0 + (h * k1 / 2.0));
+    let k3 = f(t0 + (h / 2.0), y0 + (h * k2 / 2.0));
+    let k4 = f(t0 + h, y0 + (h * k3));
+    y0 + (h / 6.0) * (k1 + (2.0 * k2) + (2.0 * k3) + k4)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::numerical_integration::{euler, rk4};
+    use nalgebra::Vector1;
+
+    #[test]
+    fn initial_value_problem_euler() {
+        let f = |_t: f64, y: Vector1<f64>| y.clone();
+        let t0 = 0.0;
+        let mut y0 = Vector1::new(1.0);
+        let h = 1.0;
+
+        y0 = euler(&f, h, t0, y0);
+        assert_eq!(y0[0], 2.0);
+        y0 = euler(&f, h, t0, y0);
+        assert_eq!(y0[0], 4.0);
+        y0 = euler(&f, h, t0, y0);
+        assert_eq!(y0[0], 8.0)
     }
-    out
+
+    #[test]
+    fn initial_value_problem_rk4() {
+        let f = |_t: f64, y: Vector1<f64>| y.clone();
+        let mut t0 = 0.0;
+        let mut y0 = Vector1::new(1.0);
+        let h = 0.1;
+
+        let mut counter = 0;
+        while counter < 10 {
+            counter += 1;
+            y0 = rk4(&f, h, t0, y0);
+            t0 += h;
+        }
+        println!("{:?}", y0)
+    }
 }
